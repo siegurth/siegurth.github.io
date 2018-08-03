35daecd2a95ab2b2b98c52f4c81f6ae7c5e7827b
siegurth <frozerth@gmail.com>
2018-08-03 14:36:28 +0300

    new project

---
+++ leshkro/js/main.js
@@ -0,0 +1,46 @@
+(function($) { 
+$(document).ready(function(){
+	var wHeight = $(window).height();
+
+	$('main section:not(:last-child)').height(wHeight);
+
+	$('.backToTop').click(function(e) {  
+		e.preventDefault();
+    $('body,html').animate({scrollTop:0},700);  
+    $('#menu').children('li:not(:first-child)').removeClass('active');
+    $('#menu').children('li:first-child').addClass('active');
+  });
+  
+  $(window).scroll(function(){
+  	var $this = $(window),
+  			posY = $this.scrollTop();
+		if (posY <= 338) {
+			$('.backToTop').hide();
+		} else {
+			$('.backToTop').show();
+		}
+  });
+
+	$(".map").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+	});
+
+	$("#menu").on("click","a", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+		top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+
+		if ($('a').hasClass('active')) {
+			event.preventDefault();
+		}
+		else {
+			$(this).parent().addClass('active').siblings().removeClass('active');
+		}
+	});
+
+});
+})(jQuery);
